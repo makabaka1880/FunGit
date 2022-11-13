494e885f00f357d67e84a77ef6c6c76de7065830
makabaka1880 <makabaka1880@outlook.com>
2022-11-13 21:09:45 +0800

    Initial Commit

---
+++ FunGit/FunGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunGitApp.swift
+//  FunGit
+//
+//  Created by SeanLi on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct FunGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
